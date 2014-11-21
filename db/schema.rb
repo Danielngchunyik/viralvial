df97ee35fadce90267bb25389df62497ed28290d
eldrethlim <eldrethlim@gmail.com>
2014-11-21 07:51:29 +0000

    add campaign model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121071720) do
+ActiveRecord::Schema.define(version: 20141121073307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "campaigns", force: true do |t|
+    t.boolean  "status"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
---
+++ spec/factories/campaigns.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :campaign do
+    status false
+start_date "2014-11-21 07:33:07"
+deadline "2014-11-21 07:33:07"
+title "MyString"
+description "MyString"
+image "MyString"
+  end
+
+end
