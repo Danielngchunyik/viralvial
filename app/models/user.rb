9ee5279b31e53b621ac3b7e5b2076ff5948bc142
eldrethlim <eldrethlim@gmail.com>
2014-11-21 07:29:50 +0000

    add user taggable to specialization and interests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 8 }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
-
   validates :email, uniqueness: true
+
+  acts_as_taggable_on :specializations, :interests
 end
