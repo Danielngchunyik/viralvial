f5e9e9de9f1567ca840639aff8f582b21b42b472
eldrethlim <eldrethlim@gmail.com>
2014-11-25 08:44:11 +0000

    update factories

--- spec/factories/announcements.rb
+++ spec/factories/announcements.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :announcement do
     message "MyText"
-title "MyString"
+    title "MyString"
   end
 
 end
--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -1,11 +1,9 @@
 FactoryGirl.define do
   factory :campaign do
     status false
-start_date "2014-11-21 07:33:07"
-deadline "2014-11-21 07:33:07"
-title "MyString"
-description "MyString"
-image "MyString"
+    start_date "2014-11-21 07:33:07"
+    deadline "2014-11-21 07:33:07"
+    title "MyString"
+    description "MyString"
   end
-
 end
