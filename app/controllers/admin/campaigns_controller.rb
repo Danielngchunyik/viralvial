35665b4738b71b7c9d75d9914ba1cc5a488f4254
eldrethlim <eldrethlim@gmail.com>
2014-11-25 04:19:29 +0000

    create basic campaigns controller, remove uneeded files

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -1,8 +1,18 @@
 class Admin::CampaignsController < ApplicationController
+  before_action :set_campaign, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, expect: [:index, :show]
+
   def index
+    @campaigns = Campaign.order("created_at DESC")
   end
 
   def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+    @campaign.save
   end
 
   def show
@@ -13,4 +23,14 @@ class Admin::CampaignsController < ApplicationController
 
   def update
   end
+
+  private
+
+    def set_campaign
+      @campaign = Campaign.find(params[:id])
+    end
+
+    def campaign_params
+      params.require(:campaign).permit(:status, :start_date, :end_date, :title, :description)
+    end
 end
