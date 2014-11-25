91af6443c07d8ede1b7027d46f7713e5d674e950
shinn <shinn@tinkerbox.com.sg>
2014-11-25 11:46:36 +0800

    Set up email sending

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,33 +1,31 @@
 class PasswordResetsController < ApplicationController
   skip_before_action :require_login
-  
+
   def create
     @user = User.find_by_email(params[:email])
 
     if @user
       @user.deliver_reset_password_instructions!
+      redirect_to(root_path, notice: 'Instructions have been sent to your email.')
+    else
+      redirect_to(root_path, notice: 'User not found!')
     end
-
-    redirect_to(root_path, notice: "Instructions have been sent to your email.")
   end
 
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
 
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    return not_authenticated if @user.blank?
   end
 
   def update
     @user.password_confirmation = params[:user][:password_confirmation]
 
     if @user.change_password!(params[:user][:password])
-      redirect_to(root_path, notice: "Password was successfully updated.")
+      redirect_to(root_path, notice: 'Password was successfully updated.')
     else
-      render action: "edit"
+      render :edit
     end
   end
 end
