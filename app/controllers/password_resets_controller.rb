4edc3c53149947a552a06b27ebcca4649b1ce358
eldrethlim <eldrethlim@gmail.com>
2014-11-25 04:11:09 +0000

    fix password reset, add email form

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,6 @@
 class PasswordResetsController < ApplicationController
   skip_before_action :require_login
+  before_action :set_token, only: [:edit, :update]
 
   def create
     @user = User.find_by_email(params[:email])
@@ -13,19 +14,27 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
 
     return not_authenticated if @user.blank?
   end
 
   def update
+    @user = User.load_from_reset_password_token(params[:id])
     @user.password_confirmation = params[:user][:password_confirmation]
 
+    return not_authenticated if @user.blank?
+    
     if @user.change_password!(params[:user][:password])
       redirect_to(root_path, notice: 'Password was successfully updated.')
     else
       render :edit
     end
   end
+
+  private
+
+    def set_token
+      @token = params[:id]
+    end
 end
