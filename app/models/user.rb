33bb7f3b777c92e765e47134b851943f5035dd36
shinn <shinn@tinkerbox.com.sg>
2014-11-26 16:35:11 +0800

    Add worker for user scoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ActiveRecord::Base
     config.authentications_class = Authentication
   end
 
-  validates :password, length: { minimum: 8 }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 8 }, if: :password
+  validates :password, confirmation: true, if: :password
+  validates :password_confirmation, presence: true, if: :password
   validates :email, uniqueness: true
 
   acts_as_taggable_on :specializations, :interests
@@ -23,37 +23,10 @@ class User < ActiveRecord::Base
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
 
-  def self.set_social_scores
-    #get user followers percentile
-    total_followers ||= User.pluck("(scores -> 'followers')::integer")
-    less_followers = []
-    same_followers = []
-
-    total_followers.each do |tf|
-      if tf < self.scores["followers"].to_i
-        less_followers.push(tf)
-      elsif tf == self.scores["followers"].to_i
-        same_followers.push(tf)
-      end
-    end
-
-    #set reach_score
-    self.reach_score ||= ((less_followers.length + (0.5 * same_followers.length))/total_followers.length * 100).round(2)
-    
-    #set sx_index
-    self.sx_index ||= ((self.localization.to_f + self.reach_score.to_f)/200*100).round(2)
-    
-    #set_influence_score
-    self.influence_score ||= ((self.klout.to_f + self.sx_index.to_f)/200*100).round(2)
-
-    #set_socialite_score
-    self.socialite_score ||= (self.influence_score.to_f + self.karma.to_f).round(2)
-  end
-
   private
 
   def update_social_scores
-    ScoresWorker.perform_async()
+    ScoresWorker.perform_async
   end
 
   def set_default_password
