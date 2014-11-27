4ce688c36f48dc33b05147e0f19ae7998731e07f
eldrethlim <eldrethlim@gmail.com>
2014-11-27 03:23:13 +0000

    add sorting asc and desc for table

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,32 @@
 class Admin::UsersController < UsersController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @users = User.order(params[:sort])
+    @users = User.order(sort_column + " " + sort_direction)
+  end
+
+  private
+
+  def sort_column
+    column_params.include?(params[:sort]) ? params[:sort] : "id"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def column_params
+    column_params = [
+      "(users.scores -> 'socialite_score')::float(2)",
+      "(users.scores -> 'karma')::float(2)",
+      "(users.scores -> 'influence_score')::float(2)",
+      "(users.scores -> 'sx_index')::float(2)",
+      "(users.scores -> 'reach_score')::float(2)",
+      "(users.scores -> 'localization')::integer",
+      "(users.scores -> 'klout')::integer",
+      "(users.scores -> 'followers')::integer",
+      "email",
+      "id",
+    ]
   end
 end
