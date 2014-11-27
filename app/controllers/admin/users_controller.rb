6d3900526ea7905cd8f10589f15dfe01a92cdf70
shinn <shinn@tinkerbox.com.sg>
2014-11-27 16:54:15 +0800

    a little bit refactoring

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,32 +1,28 @@
 class Admin::UsersController < UsersController
-  helper_method :sort_column, :sort_direction
+  helper_method :sort_direction
 
   def index
-    @users = User.order(sort_column + " " + sort_direction)
+    @users = User.order("#{column_params[params[:sort].to_s] || 'id'} #{sort_direction}")
   end
 
   private
 
-  def sort_column
-    column_params.include?(params[:sort]) ? params[:sort] : "id"
-  end
-
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : 'asc'
   end
 
   def column_params
-    column_params = [
-      "(users.scores -> 'socialite_score')::float(2)",
-      "(users.scores -> 'karma')::float(2)",
-      "(users.scores -> 'influence_score')::float(2)",
-      "(users.scores -> 'sx_index')::float(2)",
-      "(users.scores -> 'reach_score')::float(2)",
-      "(users.scores -> 'localization')::integer",
-      "(users.scores -> 'klout')::integer",
-      "(users.scores -> 'followers')::integer",
-      "email",
-      "id",
-    ]
+    {
+      'socialite' => "(users.scores -> 'socialite_score')::float(2)",
+      'karma' => "(users.scores -> 'karma')::float(2)",
+      'influence' => "(users.scores -> 'influence_score')::float(2)",
+      'sx' => "(users.scores -> 'sx_index')::float(2)",
+      'reach' => "(users.scores -> 'reach_score')::float(2)",
+      'localization' => "(users.scores -> 'localization')::integer",
+      'klout' => "(users.scores -> 'klout')::integer",
+      'followers' => "(users.scores -> 'followers')::integer",
+      'email' => 'email',
+      'id' => 'id'
+    }
   end
 end
