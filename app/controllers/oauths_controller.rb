56db22f6e2e5a1293bfc719ea13bae8ce27b3337
eldrethlim <eldrethlim@gmail.com>
2014-12-05 16:10:12 +0800

    complete country and location integration

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -28,7 +28,8 @@ class OauthsController < ApplicationController
   def save_user_details
     fb_user = FbGraph::User.fetch("me?access_token=#{@token}")
     @user.remote_image_url = "#{fb_user.picture}?redirect=1&height=300&type=normal&width=300"
-    @user.update(birthday: fb_user.birthday)
+    location_array = fb_user.location.name.split(', ')
+    @user.update(birthday: fb_user.birthday, location: location_array[0], country: IsoCountryCodes.search_by_name(location_array[1])[0].alpha2)
   end
 
   def get_access_token
