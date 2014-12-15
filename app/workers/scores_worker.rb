4b465a55024f997bdc2f9780472dc4493a9934c0
eldrethlim <eldrethlim@gmail.com>
2014-12-15 12:41:51 +0800

    refactored and fix error with scores_worker

--- app/workers/scores_worker.rb
+++ app/workers/scores_worker.rb
@@ -8,12 +8,7 @@ class ScoresWorker
       if user.followers.present?
         less_followers, same_followers = split_followers(total_followers, user)
 
-        user.reach_score = ((less_followers.length + (0.5 * same_followers.length))/total_followers.length * 100).round(2)
-        user.sx_index = ((user.localization.to_f + user.reach_score.to_f)/200*100).round(2)
-        user.influence_score = ((user.klout.to_f + user.sx_index.to_f)/200*100).round(2)
-        user.socialite_score = (user.influence_score.to_f + user.karma.to_f).round(2)
-        # TODO: https://github.com/rails/rails/pull/15674
-        user.scores_will_change!
+        update_scores(user, less_followers, same_followers, total_followers)
         user.save
       end
     end
@@ -21,13 +16,22 @@ class ScoresWorker
 
   private
 
+  def update_scores(user, less_followers, same_followers, total_followers)
+    user.reach_score = ((less_followers.length + (0.5 * same_followers.length))/total_followers.length * 100).round(2)
+    user.sx_index = ((user.localization.to_f + user.reach_score.to_f)/200*100).round(2)
+    user.influence_score = ((user.klout.to_f + user.sx_index.to_f)/200*100).round(2)
+    user.socialite_score = (user.influence_score.to_f + user.karma.to_f).round(2)
+  end
+
+
   def split_followers(followers, user)
-    less, same = []
-    followers.each do |tf|
-      if tf < user.followers.to_i
-        less << tf
-      elsif tf == user.followers.to_i
-        same << tf
+    less = []
+    same = []
+    followers.each do |follower|
+      if follower < user.followers.to_i
+        less << follower
+      elsif follower == user.followers.to_i
+        same << follower
       end
     end
     [less, same]
