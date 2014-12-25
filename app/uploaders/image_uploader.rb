388f6980a3f02849569d3664f01a7741f6364a7e
eldrethlim <eldrethlim@gmail.com>
2014-12-25 15:04:45 +0000

    removed profile image version, reduce redundancy

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,10 +40,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [150, 150]
   end
 
-  version :profile do
-    process :resize_to_fit => [100,100]
-  end
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
