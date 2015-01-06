b7bebeb965b58e1b3a3aae738f1309e7c0122c4b
eldrethlim <eldrethlim@gmail.com>
2015-01-06 11:46:10 +0800

    fix twitter sorcery error

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Campaign, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Campaign do
+  describe '#categories?' do
+    it "should return true when category_list is blank" do
+      campaign = Campaign.new(start_date: Date.today, end_date: Date.today)
+      user = User.new
+      expect(campaign.conditions?(user)).to eq true
+    end
+  end
 end
