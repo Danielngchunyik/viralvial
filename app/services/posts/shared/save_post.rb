b9cfd017710ae35438195dda8e811a5a62d620c3
eldrethlim <eldrethlim@gmail.com>
2015-01-07 17:22:52 +0800

    minor save-post-service refactor

--- app/services/posts/shared/save_post.rb
+++ app/services/posts/shared/save_post.rb
@@ -1,10 +1,10 @@
 module Posts::Shared::SavePost
 
   def save_post!(ext_id, ext_type)
-    @post = @current_user.posts.build(@post_params)
-    @post.external_post_id = ext_id
-    @post.external_post_id_type = ext_type
-    @post.campaign_id = @campaign_id
+    @post = @current_user.posts.build(@post_params.merge(external_post_id: ext_id, 
+                                                         external_post_id_type: ext_type,
+                                                         campaign_id: @campaign_id
+                                                         ))
     @post.save
   end
 end
