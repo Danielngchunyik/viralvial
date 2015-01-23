bb16dc8666646de7f1e95be7878ce2edc3b23818
eldrethlim <eldrethlim@gmail.com>
2015-01-23 16:25:36 +0800

    fix twitter

--- app/services/oauth/retrieve_twitter_user_info.rb
+++ app/services/oauth/retrieve_twitter_user_info.rb
@@ -8,8 +8,10 @@ class Oauth::RetrieveTwitterUserInfo
     @screen_name = screen_name
   end
 
+
+
   def save
-    @twitter = Posts::InitializeTwitterClient.new(@token, @secret)
+    @twitter = initialize_twitter_client(@token, @secret)
 
     twitter_user = @twitter.client.user(@screen_name)
     upload_profile_image(twitter_user)
@@ -22,5 +24,19 @@ class Oauth::RetrieveTwitterUserInfo
   def upload_profile_image(user)
     image_extract = user.profile_image_url_https.to_str.split("__")
     @user.remote_image_url = "#{image_extract[0]}__200x200.jpeg"
+  end
+
+  def initialize_twitter_client(token, secret)
+    @consumer_key = ENV['TWITTER_KEY']
+    @consumer_secret = ENV['TWITTER_SECRET']
+    @access_token = token
+    @access_token_secret = secret
+
+    @twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key = @consumer_key
+      config.consumer_secret = @consumer_secret
+      config.access_token = @access_token
+      config.access_token_secret = @access_token_secret
+    end
   end
 end
