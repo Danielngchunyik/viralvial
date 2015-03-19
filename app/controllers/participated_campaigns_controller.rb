94c44893f94e005f230080d2f8a4d9d44f5120f5
eldrethlim <eldrethlim@gmail.com>
2015-03-19 23:08:30 +0800

    fix campaigns controller

--- app/controllers/participated_campaigns_controller.rb
+++ app/controllers/participated_campaigns_controller.rb
@@ -3,11 +3,13 @@ class ParticipatedCampaignsController < ApplicationController
   respond_to :html, :js
 
   def index
-    @campaigns = []
+    campaigns = []
 
     current_user.posts.each do |post|
-      @campaigns << post.campaign
+      campaigns << post.campaign
     end
+
+    @campaigns = CampaignDecorator.wrap(campaigns)
 
     respond_with(@campaigns)
   end
