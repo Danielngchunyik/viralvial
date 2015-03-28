73dccebe6131a752f6a51e4ce420bcbe2d03eadf
eldrethlim <eldrethlim@gmail.com>
2015-03-28 08:43:40 +0800

    smoothen out edit user animations

--- app/assets/javascripts/users/_edit_user.js
+++ app/assets/javascripts/users/_edit_user.js
@@ -2,14 +2,14 @@ var initBIP = function() {
   
   // Successful update
   $(".best_in_place").best_in_place().bind('ajax:success', function() {
-    $(this).transition('pulse')
+    runAnimations(this, '', 'pulse');
     $('#profile_user_name_display').html($('#profile_user_name').html());
     $('#header-user-name').html($('#profile_user_name').html());
   });
 
   // Failed update
   $(".best_in_place").best_in_place().bind('ajax:error', function() {
-    $(this).transition({ animation: 'shake', duration: '0.5s' })
+    runAnimations(this, '', 'shake');
   })
 }
 
