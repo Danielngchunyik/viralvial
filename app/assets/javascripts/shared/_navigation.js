d70e6b4e94a1713e16861193c885f3ab928c9d0e
eldrethlim <eldrethlim@gmail.com>
2015-03-29 17:54:12 +0800

    added ajax to pagination links

--- app/assets/javascripts/shared/_navigation.js
+++ app/assets/javascripts/shared/_navigation.js
@@ -1,3 +1,13 @@
+// Add PJAX attribute on pagination links
+var ajaxPaginationLinks = function() {
+  [].slice.call($('.pagination a')).forEach(function(el) {
+    $(el).attr('data-run-pjax', true);
+  });
+}
+
 $(function() {
-  $(document).pjax('a:not([data-remote]):not([data-behavior]):not([data-skip-pjax])', '[data-pjax-container]')
+  $(document).pjax('a[data-run-pjax]', '[data-pjax-container]');
 });
+
+$(document).ready(ajaxPaginationLinks);
+$(document).ajaxComplete(ajaxPaginationLinks);
